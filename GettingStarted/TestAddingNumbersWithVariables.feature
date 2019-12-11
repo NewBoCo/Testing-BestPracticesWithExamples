740ee3fec2d6e5462f8b312365e6d83cf7640816
Nate Adams <psiberknetic@gmail.com>
2019-12-11 15:24:11 -0600

    This time keep the code SpecFlow generated

---
+++ GettingStarted/TestAddingNumbersWithVariables.feature
@@ -0,0 +1,9 @@
+﻿Feature: TestAddingNumbersWithVariables
+	In order to avoid writing a lot of extra testing code
+	I want to be able to use variables in my SpecFlow tests
+
+@mytag
+Scenario: Add 1 and 2
+	Given I have the numbers 1 and 2
+	When I add them together
+	Then they should add up to 3
---
+++ GettingStarted/TestAddingNumbersWithVariablesSteps.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using TechTalk.SpecFlow;
+
+namespace GettingStarted
+{
+    [Binding]
+    public class TestAddingNumbersWithVariablesSteps
+    {
+        [Given(@"I have the numbers (.*) and (.*)")]
+        public void GivenIHaveTheNumbersAnd(int p0, int p1)
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [When(@"I add them together")]
+        public void WhenIAddThemTogether()
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [Then(@"they should add up to (.*)")]
+        public void ThenTheyShouldAddUpTo(int p0)
+        {
+            ScenarioContext.Current.Pending();
+        }
+    }
+}
