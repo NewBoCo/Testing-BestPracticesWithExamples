d8bd5ccb45953781abf2cd1bc28af84c043709bc
Nate Adams <psiberknetic@gmail.com>
2019-12-11 14:48:57 -0600

    Add project files.

---
+++ GettingStarted/1_SimpleStarterTests.feature
@@ -0,0 +1,10 @@
+﻿Feature: Add Two Numbers Together
+	As someone who likes adding numbers together
+	When I add two numbers together
+	I get the correct results
+
+@mytag
+Scenario: Add two numbers - no variables
+	Given I have entered the numbers 1 and 2
+	When I add 1 and 2 together
+	Then the result should be 3
---
+++ GettingStarted/AddTwoNumbersTogetherSteps.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using TechTalk.SpecFlow;
+
+namespace GettingStarted
+{
+    [Binding]
+    public class AddTwoNumbersTogetherSteps
+    {
+        [Given(@"I have entered the numbers (.*) and (.*)")]
+        public void GivenIHaveEnteredTheNumbersAnd(int p0, int p1)
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [When(@"I add (.*) and (.*) together")]
+        public void WhenIAddAndTogether(int p0, int p1)
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [Then(@"the result should be (.*)")]
+        public void ThenTheResultShouldBe(int p0)
+        {
+            ScenarioContext.Current.Pending();
+        }
+    }
+}
