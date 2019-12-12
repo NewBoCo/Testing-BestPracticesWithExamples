1dbf4e116ca2483bd9ce972eb0e694cce261262d
Nate Adams <psiberknetic@gmail.com>
2019-12-12 09:07:38 -0600

    Simply test scenario file with outline

---
+++ GettingStarted/CheckToSeeIfTheStoreIsOpen_OutlineVersionSteps.cs
@@ -0,0 +1,32 @@
+﻿using FluentAssertions;
+using SomeStuffToTest;
+using System;
+using TechTalk.SpecFlow;
+
+namespace GettingStarted
+{
+    [Binding]
+    public class CheckToSeeIfTheStoreIsOpen_OutlineVersionSteps
+    {
+        DateTime dateToTest;
+
+        [Given(@"it is a certain (.*) and (.*)")]
+        public void GivenItIsACertainAnd(string p0, string p1)
+        {
+            dateToTest = DateTime.Parse($"{p0} {p1}");
+        }
+
+        [Then(@"The store (.*) open at that time")]
+        public void ThenTheStoreOpen(string p0)
+        {
+            if (dateToTest.IsStoreOpen())
+            {
+                p0.ToLower().Replace(" ", "").Should().Be("is");
+            }
+            else
+            {
+                p0.ToLower().Replace(" ", "").Should().Be("isnot");
+            }
+        }
+    }
+}
---
+++ GettingStarted/IsStoreOpenScenarioOutlineVersion.feature
@@ -0,0 +1,24 @@
+﻿Feature: Check to see if the store is open - outline version
+	As a potential customer
+	I want to know if your store is open
+	So I don't waste my time driving there if it isn't
+	Store Hours:
+	M-Th 8-10
+	Fri 8-11
+	Sat 10-8
+	Sun 10-6
+
+Scenario Outline: Testing multiple dates and times
+	Given it is a certain <date> and <time>
+	Then The store <isornot> open at that time
+
+	Examples:
+		| date       | time     | isornot |
+		| 12/20/2019 | 12:00 PM | is      |
+		| 12/12/2019 | 9:00 AM  | is      |
+		| 01/03/2019 | 6:00 AM  | isnot   |
+		| 12/12/2019 | 11:00 PM | isnot   |
+		| 12/14/2019 | 9:00 AM  | isnot   |
+		| 12/14/2019 | 11:00 AM | is      |
+		| 12/12/2019 | 8:00 AM  | is      |
+		| 12/12/2019 | 10:00 PM | isnot   |
